46cbab968c57a570e5e53a7de2babd5dacdfe4c0
Rustam Zaitov <rustam.zaitov@xamarin.com>
2014-09-23 17:43:19 +0400

    remove unused code

--- FindDerived/FindDerived/Program.cs
+++ FindDerived/FindDerived/Program.cs
@@ -9,31 +9,15 @@ namespace FindDerived
 {
 	class MainClass
 	{
+		const string XamarinIOS = "/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/Xamarin.iOS.dll";
+		const string Monotouch = "/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/2.1/monotouch.dll";
+
 		public static void Main (string[] args)
 		{
-			/*
-			Assembly asm = Assembly.LoadFrom ("monotouch.dll");
-			IEnumerable<Type> types = asm.GetLoadableTypes ();
-
-			Type dicContainerType = types.Where (t => t.Name == "DictionaryContainer").First ();
-
-			foreach (var t in types) {
-				if(t.IsSubclassOf(dicContainerType))
-					Console.WriteLine (t.Name);
-			}
-			*/
-
-			/*
-			ModuleDefinition mtouchDef = ModuleDefinition.ReadModule ("monotouch.dll");
-			foreach (TypeDefinition typeDef in mtouchDef.Types) {
-				Console.WriteLine (typeDef.FullName);
-			}
-			*/
-
-//			ModuleDefinition mtouchDef = ModuleDefinition.ReadModule ("monotouch.dll");
-			ModuleDefinition mtouchDef = ModuleDefinition.ReadModule ("Xamarin.iOS.dll");
+//			ModuleDefinition mtouchDef = ModuleDefinition.ReadModule (Monotouch);
+			ModuleDefinition moduleDef = ModuleDefinition.ReadModule (XamarinIOS);
 			Printer printer = new Printer ();
-			ModuleHandler handler = new ModuleHandler (mtouchDef);
+			ModuleHandler handler = new ModuleHandler (moduleDef);
 //			handler.FindDerivedFrom ("MonoTouch.Foundation.DictionaryContainer");
 //			handler.FindDerivedFrom ("MonoTouch.CoreVideo.CVPixelBufferAttributes");
 //			handler.FindMethodWithParameterName ("options");
@@ -46,29 +30,11 @@ namespace FindDerived
 
 
 			PropertyDefinition prop = handler.FindProperty ("UIKit.UIImagePickerController", "OriginalImage");
-			PropertyUsageFinder pUsageFinder = new PropertyUsageFinder (mtouchDef, prop);
+			PropertyUsageFinder pUsageFinder = new PropertyUsageFinder (moduleDef, prop);
 			IEnumerable<MethodReference> callers = pUsageFinder.Find ();
 
 			foreach (var mRef in callers)
 				printer.Print (mRef);
 		}
 	}
-
-	/*
-	static class Ext
-	{
-		public static IEnumerable<Type> GetLoadableTypes(this Assembly assembly)
-		{
-			if (assembly == null) throw new ArgumentNullException("assembly");
-			try
-			{
-				return assembly.GetTypes();
-			}
-			catch (ReflectionTypeLoadException e)
-			{
-				return e.Types.Where(t => t != null);
-			}
-		}
-	}
-	*/
 }
